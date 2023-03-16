722ba6b86cf68fb2e6abe53f447fdb93844ed581
hariom-ojha23 <hariomojha2301@gmail.com>
2023-03-16 16:10:39 +0530

    changing column width in pdf

--- backend/src/client_info/services/client_info/client_info.service.ts
+++ backend/src/client_info/services/client_info/client_info.service.ts
@@ -1,64 +1,100 @@
 import { Injectable } from '@nestjs/common';
-import {InjectRepository} from '@nestjs/typeorm'
-import {Repository} from 'typeorm'
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { ClientInfo } from 'src/typeorm/entities/ClientInfo';
 import { AddClientInfoType } from 'src/utils/types';
 
-const PDFDocument = require("pdfkit-table")
+const PDFDocument = require('pdfkit-table');
 
 @Injectable()
 export class ClientInfoService {
-
-  constructor(@InjectRepository(ClientInfo) private clientInfoRepo: Repository<ClientInfo>) {}
+  constructor(
+    @InjectRepository(ClientInfo)
+    private clientInfoRepo: Repository<ClientInfo>,
+  ) {}
 
   getAllClientInfo() {
-    return this.clientInfoRepo.find()
+    return this.clientInfoRepo.find();
   }
 
   addClientInfo(clientData: AddClientInfoType) {
-    const newData = this.clientInfoRepo.create(clientData)
-    return this.clientInfoRepo.save(newData)
+    const newData = this.clientInfoRepo.create(clientData);
+    return this.clientInfoRepo.save(newData);
   }
 
   async generatePDF(): Promise<Buffer> {
-    const clientList = await this.clientInfoRepo.find()
+    const clientList = await this.clientInfoRepo.find();
     const pdfBuffer: Buffer = await new Promise((resolve) => {
-      let doc = new PDFDocument({ margin: 30, size: 'A4', bufferPages: true })
+      let doc = new PDFDocument({ margin: 30, size: 'A4', bufferPages: true });
 
       // todo
       const table = {
-        title: {label: "Client Info", fontSize: 14,},
+        title: { label: 'Client Info', fontSize: 14 },
         headers: [
-          { label: "Id", property: 'id', width: 80, valign: 'center', align: "left", padding: 12, headerColor:"#2196f3", headerOpacity:0.5 },
-          { label: "Name", property: 'name', width: 100, valign: 'center', align: "right", padding: 12, headerColor:"#2196f3", headerOpacity:0.5  }, 
-          { label: "Email Address", property: 'email', width: 200, valign: 'center', align: "right", padding: 12, headerColor:"#2196f3", headerOpacity:0.5  }, 
-          { label: "Phone Number", property: 'phone', width: 150, valign: 'center', align: "right", padding: 12, headerColor:"#2196f3", headerOpacity:0.5  }, 
+          {
+            label: 'Id',
+            property: 'id',
+            width: 40,
+            valign: 'center',
+            align: 'left',
+            padding: 12,
+            headerColor: '#2196f3',
+            headerOpacity: 0.5,
+          },
+          {
+            label: 'Name',
+            property: 'name',
+            width: 140,
+            valign: 'center',
+            align: 'right',
+            padding: 12,
+            headerColor: '#2196f3',
+            headerOpacity: 0.5,
+          },
+          {
+            label: 'Email Address',
+            property: 'email',
+            width: 200,
+            valign: 'center',
+            align: 'right',
+            padding: 12,
+            headerColor: '#2196f3',
+            headerOpacity: 0.5,
+          },
+          {
+            label: 'Phone Number',
+            property: 'phone',
+            width: 150,
+            valign: 'center',
+            align: 'right',
+            padding: 12,
+            headerColor: '#2196f3',
+            headerOpacity: 0.5,
+          },
         ],
         datas: clientList,
         options: {
           minRowHeight: 30,
-        }
+        },
       };
 
-
       doc.table(table, {
-        prepareHeader: () => doc.font("Helvetica-Bold").fontSize(12),
+        prepareHeader: () => doc.font('Helvetica-Bold').fontSize(12),
         prepareRow: (row, indexColumn, indexRow, rectRow, rectCell) => {
-          doc.font("Helvetica").fontSize(11);
+          doc.font('Helvetica').fontSize(11);
           indexColumn === -1 && doc.addBackground(rectRow, '#f5f5f5', 1);
         },
       });
 
-
-      let buffer = []
-      doc.on('data', buffer.push.bind(buffer))
+      let buffer = [];
+      doc.on('data', buffer.push.bind(buffer));
       doc.on('end', () => {
-        const data = Buffer.concat(buffer)
-        resolve(data)
-      })
-      doc.end()
-    })
+        const data = Buffer.concat(buffer);
+        resolve(data);
+      });
+      doc.end();
+    });
 
-    return pdfBuffer
+    return pdfBuffer;
   }
 }
